ee4cf293cad649403f6928b0de5cccb122655667
almeidaqarafael <rafaeltesterqa@gmail.com>
2024-08-08 11:47:24 -0300

    Adicionando teste de logout

---
+++ cypress/e2e/gui/logout.cy.js
@@ -0,0 +1,14 @@
+describe('Logout', () => {
+  
+  beforeEach(() => {
+    cy.login()
+    cy.visit('/')
+  })
+
+  it('sucesso', () => {
+    cy.logout()
+
+    cy.url().should('be.equal', `${Cypress.config('baseUrl')}/users/sign_in`)
+  })
+
+})
